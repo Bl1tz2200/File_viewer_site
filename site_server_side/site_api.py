b78d0c12418dbef93a7c9635507523cdb3b8da1e
Bl1tz2200 <154754925+Bl1tz2200@users.noreply.github.com>
2024-06-15 12:11:47 +0300

    Add files via upload

---
+++ site_server_side/site_api.py
@@ -0,0 +1,41 @@
+#!flask/bin/python
+from flask import Flask, render_template, request, make_response, Response
+import os
+
+app = Flask(__name__)
+
+app.config['UPLOAD_FOLDER'] = '/var/www/html/data'
+
+@app.route('/upload-file', methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+        response = Response("File upload successfully")
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        return response
+
+@app.route('/get-files', methods=['GET'])
+def get_files():
+    if request.method == 'GET':
+        files = ""
+        files_list = os.listdir(app.config['UPLOAD_FOLDER']) 
+        for i in files_list:
+            files = files + i + ";"
+        if files != "":
+            arr = Response(files)
+        else:
+            arr = Response("No files found")
+        arr.headers['Access-Control-Allow-Origin'] = '*'
+        return arr
+
+@app.route('/remove-file/<string:file>', methods=['POST'])
+def remove_files(file):
+    if request.method == 'POST':
+        os.remove(app.config['UPLOAD_FOLDER'] + "/" + file)
+        response = Response("File deleted successfully")
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        return response
+
+if __name__ == "__main__":
+    app.run(host="25.60.122.48", port=8080)
